f39599eee761ff0df9e1ee3256fdb947290b8c6e
GEON AN <geon0078@g.eulji.ac.kr>
2025-08-26 17:36:21 +0900

    [Login] 로그인 컴포넌트 인덱스 파일 업데이트
    - 새로 추가된 회원가입 컴포넌트들 export 추가
    - 컴포넌트 모듈 구조 정리

--- src/components/login/index.ts
+++ src/components/login/index.ts
@@ -1,3 +1,4 @@
 export { default as LoginComponent } from './LoginComponent.vue';
 export { default as SignupComponent } from './SignupComponent.vue';
+export { default as SignupPersonalInfoFormComponent } from './SignupPersonalInfoFormComponent.vue';
 export { default as KakaoLoginButton } from './KakaoLoginButton.vue';
