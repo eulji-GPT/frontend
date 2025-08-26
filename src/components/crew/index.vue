a883294d8c11bc2fd50f1959d3eb8e8a8429e097
GEON AN <geon0078@g.eulji.ac.kr>
2025-08-26 17:35:46 +0900

    [Crew] 크루 인덱스 컴포넌트 업데이트
    - 크루 메인 페이지 컴포넌트 구조 개선
    - 새로운 크루 컴포넌트들과 연동

--- src/components/crew/index.vue
+++ src/components/crew/index.vue
@@ -1,41 +1,20 @@
 <template>
   <div class="crew-page">
-    <!-- Desktop21 - Hero Section -->
-    <Desktop21 />
+    <Crew />
+    <CrewRecruitment />
+    <CrewFooter />
   </div>
 </template>
 
-<script>
-import Desktop21 from './Desktop21.vue'
-
-export default {
-  name: 'CrewIndexPage',
-  components: {
-    Desktop21
-  },
-  mounted() {
-    console.log('Crew index page mounted successfully')
-  }
-}
+<script setup lang="ts">
+import Crew from './crew.vue'
+import CrewRecruitment from './CrewRecruitment.vue'
+import CrewFooter from './CrewFooter.vue'
 </script>
 
 <style scoped>
 .crew-page {
-  width: 100vw;
-  min-height: 100vh;
-  background: #141d30;
-  overflow-x: hidden;
-  display: flex;
-  flex-direction: column;
-}
-
-/* 각 섹션이 자연스럽게 이어지도록 */
-.crew-page > * {
   width: 100%;
-  margin: 0;
-  flex-shrink: 0;
-  /* 디버깅용 테두리 - 각 컴포넌트가 보이도록 */
-  border: 2px solid red;
-  min-height: 200px;
+  min-height: 100vh;
 }
 </style>
